7fb6a75e2fa8a82871b7f6e85df2deee364a8e49
p-kossa <kevossa526@gmail.com>
2018-07-25 14:38:38 -0400

    X and O props and taking turns

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,33 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-        };
-    }
-
-    render() {
-      return (
-        <button 
-            className="square" 
-            onClick={() => this.setState({value: 'X'})}
-        >
-          {this.state.value}
-        </button>
-      );
-    }
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null), // creating array 'squares'
+        xIsNext: true, // for players taking turns
+      }
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice(); 
+      // creates copy of squares array so we don't have to modify the original array
+      squares[i] = this.state.xIsNext ? 'X' : 'O'; 
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      })
+    }
+
     renderSquare(i) {
-      return <Square value={this.state.squares[i]} />;
+      return <Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)} 
+              />;
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
